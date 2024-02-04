5931e22d1d2f7d0d5a0c025affcf2ffe0cd6ee86
MD SAMIM HOSSAIN SUJON <98223435+samimhossainsujon@users.noreply.github.com>
2024-02-04 18:06:08 +0600

    code add

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Dimensions } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import Navigationbar from './src/components/navigationbar/Navigationbar';
 
+const { height, width } = Dimensions.get('window');
+
 const App = () => {
+
+  // ===============================================
+  // Hide the splash screen when the component mounts
+  // ================================================
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+
+
   return (
+
     <>
       <Navigationbar />
     </>
@@ -10,3 +25,6 @@ const App = () => {
 };
 
 export default App;
+
+
+
